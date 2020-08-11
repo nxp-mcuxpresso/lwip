c692e58e020fcdf0e1c5605fb695b0eeb7d7ebc6
xueyunfei <xueyunfei@espressif.com>
2020-08-11 15:59:22 +0800

    ip6: Add on-demand timer config for IP6 reassembly timer
    2.1.3-esp: 9813ea9a lwip:optimization lwip ip6 reassembly timer
    
    Cherry-picked b1ceb9d082c77be9c0ae1b67e3c8719655843940 from ESP.
    Signed-off-by: Stanislav Poboril <stanislav.poboril@nxp.com>

--- contrib/examples/example_app/lwipopts.h
+++ contrib/examples/example_app/lwipopts.h
@@ -329,6 +329,7 @@ void lwip_example_app_platform_assert(const char *msg, int line, const char *fil
 #define ESP_LWIP_DHCP_FINE_TIMERS_ONDEMAND 1
 #define ESP_LWIP_DNS_TIMERS_ONDEMAND 1
 #define ESP_LWIP_IP4_REASSEMBLY_TIMERS_ONDEMAND 1
+#define ESP_LWIP_IP6_REASSEMBLY_TIMERS_ONDEMAND 1
 
 
 #endif /* LWIP_LWIPOPTS_H */
