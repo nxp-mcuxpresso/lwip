a23df5e1ee6cfc23b1109fbe82efc6947fc08de2
Hamza Hajeir <hahajeir@gmail.com>
2023-06-24 20:33:01 +0300

    altcp_tls_mbedtls: Fix abort management. Fix to return ERR_ABRT on altcp_abort(). Add check to altcp_mbedtls_abort().

--- src/apps/altcp_tls/altcp_tls_mbedtls.c
+++ src/apps/altcp_tls/altcp_tls_mbedtls.c
@@ -308,6 +308,7 @@ altcp_mbedtls_lower_recv_process(struct altcp_pcb *conn, altcp_mbedtls_state_t *
 
       if (altcp_close(conn) != ERR_OK) {
         altcp_abort(conn);
+        return ERR_ABRT;
       }
       return ERR_OK;
     }
@@ -1154,8 +1155,9 @@ altcp_mbedtls_listen(struct altcp_pcb *conn, u8_t backlog, err_t *err)
 static void
 altcp_mbedtls_abort(struct altcp_pcb *conn)
 {
-  if (conn != NULL) {
+  if (conn != NULL && conn->inner_conn != NULL) {
     altcp_abort(conn->inner_conn);
+    conn->inner_conn = NULL;
   }
 }
 
